5665f25d4a837f06768a13ad0a0fd8d1e6260548
Jun Aruga <junaruga@users.noreply.github.com>
2019-10-22 16:37:38 +0200

    Fix tests. (#2)

--- project/tests/test_main.py
+++ project/tests/test_main.py
@@ -11,14 +11,8 @@ class TestMainBlueprint(BaseTestCase):
         # Ensure Flask is setup.
         response = self.client.get("/", follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Welcome!", response.data)
-        self.assertIn(b"Login", response.data)
-
-    def test_about(self):
-        # Ensure about route behaves correctly.
-        response = self.client.get("/about", follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b"About", response.data)
+        self.assertIn(b"Fedora Packages", response.data)
+        self.assertIn(b"Search", response.data)
 
     def test_404(self):
         # Ensure 404 error is handled.
