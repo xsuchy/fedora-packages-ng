953599fa705f3ed519828b1dba3f3e1b81be8a14
Miroslav Suchý <msuchy@redhat.com>
2019-10-22 15:58:57 +0200

    create /packages/

--- project/server/main/views.py
+++ project/server/main/views.py
@@ -12,6 +12,10 @@ def home():
     return render_template("main/home.html")
 
 
-@main_blueprint.route("/about/")
-def about():
-    return render_template("main/about.html")
+@main_blueprint.route("/packages/")
+@main_blueprint.route("/packages/<package_name>/")
+def packages(package_name=None):
+    if not package_name:
+        return home()
+    else:
+        return render_template("main/packages.html")
