97a5adfe1d5ca52339b36e789c0ddd97588885c6
Miroslav Suchý <msuchy@redhat.com>
2019-10-25 16:28:53 +0200

    fix travis test

--- project/tests/test__config.py
+++ project/tests/test__config.py
@@ -31,7 +31,6 @@ class TestTestingConfig(TestCase):
 
     def test_app_is_testing(self):
         self.assertTrue(current_app.config["TESTING"])
-        self.assertTrue(app.config["BCRYPT_LOG_ROUNDS"] == 4)
         self.assertTrue(app.config["WTF_CSRF_ENABLED"] is False)
 
 
@@ -44,7 +43,6 @@ class TestProductionConfig(TestCase):
         self.assertFalse(current_app.config["TESTING"])
         self.assertTrue(app.config["DEBUG_TB_ENABLED"] is False)
         self.assertTrue(app.config["WTF_CSRF_ENABLED"] is True)
-        self.assertTrue(app.config["BCRYPT_LOG_ROUNDS"] == 13)
 
     def test_secret_key_has_been_set(self):
         self.assertTrue(
