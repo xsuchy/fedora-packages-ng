ef5fecb4dbc5cd0f4663562a61b9fb0a5b5fa7dc
Jakub Kadlcik <frostyx@email.cz>
2020-05-24 22:43:27 +0200

    Rewrite the package overview page from the previous markup to jinja2
    Still passing mocked values into the page.

--- fedoracommunity/server/models.py
+++ fedoracommunity/server/models.py
@@ -6,6 +6,11 @@ class Package:
         self.summary = summary
         self.subpackages = subpackages
 
+        # TODO
+        self.description = "This is a description for {0} package".format(name)
+        self.homepage = "https://foo.bar/baz"
+        self.owner = Owner("msuchy")
+
     @property
     def url(self):
         return "/packages/" + self.name
@@ -19,3 +24,19 @@ class Package:
     def is_subpackage(self):
         # @TODO proper implementation
         return self.name.startswith("sub")
+
+    @property
+    def latest_build(self):
+        # @TODO proper implementation
+        return "123-1.fc33"
+
+
+class Owner:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def url(self):
+        if self.name.endswith("-maint"):
+            return "https://admin.fedoraproject.org/pkgdb/users/packages/{0}".format(self.name)
+        return "https://fedoraproject.org/wiki/User:{0}".format(self.name)
--- fedoracommunity/server/packages.py
+++ fedoracommunity/server/packages.py
@@ -11,4 +11,5 @@ def get_packages():
 
 # @TODO have some real logic function for this
 def get_package(package_name):
-    return Package(package_name, "Summary for {0} package".format(package_name))
+    return Package(package_name, "Summary for {0} package".format(package_name),
+                   [Package("sub1", "subsum1"), Package("sub2", "subsum2")])
